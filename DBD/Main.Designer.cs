e6f125e0c6282baa03a45f03235ca93087cc3760
Tasin <Tasin-a@hotmail.com>
2017-05-22 12:32:05 +0200

    SomeChanges

--- DBD/Main.Designer.cs
+++ DBD/Main.Designer.cs
@@ -48,41 +48,42 @@
             this.panel3 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.btnReCreate = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // tbStoredProcedure
             // 
             this.tbStoredProcedure.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbStoredProcedure.Location = new System.Drawing.Point(219, 377);
-            this.tbStoredProcedure.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbStoredProcedure.Location = new System.Drawing.Point(164, 276);
             this.tbStoredProcedure.Name = "tbStoredProcedure";
-            this.tbStoredProcedure.Size = new System.Drawing.Size(465, 30);
+            this.tbStoredProcedure.Size = new System.Drawing.Size(350, 26);
             this.tbStoredProcedure.TabIndex = 22;
             // 
             // tbRegularExpression
             // 
             this.tbRegularExpression.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbRegularExpression.Location = new System.Drawing.Point(219, 281);
-            this.tbRegularExpression.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbRegularExpression.Location = new System.Drawing.Point(164, 215);
             this.tbRegularExpression.Name = "tbRegularExpression";
-            this.tbRegularExpression.Size = new System.Drawing.Size(465, 30);
+            this.tbRegularExpression.Size = new System.Drawing.Size(350, 26);
             this.tbRegularExpression.TabIndex = 21;
             // 
             // tbPreparedStatement
             // 
             this.tbPreparedStatement.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbPreparedStatement.Location = new System.Drawing.Point(219, 199);
-            this.tbPreparedStatement.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbPreparedStatement.Location = new System.Drawing.Point(164, 153);
             this.tbPreparedStatement.Name = "tbPreparedStatement";
-            this.tbPreparedStatement.Size = new System.Drawing.Size(465, 30);
+            this.tbPreparedStatement.Size = new System.Drawing.Size(350, 26);
             this.tbPreparedStatement.TabIndex = 20;
             // 
             // btnPreparedStatement
             // 
-            this.btnPreparedStatement.Location = new System.Drawing.Point(713, 192);
-            this.btnPreparedStatement.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnPreparedStatement.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPreparedStatement.Location = new System.Drawing.Point(535, 147);
             this.btnPreparedStatement.Name = "btnPreparedStatement";
-            this.btnPreparedStatement.Size = new System.Drawing.Size(160, 49);
+            this.btnPreparedStatement.Size = new System.Drawing.Size(120, 40);
             this.btnPreparedStatement.TabIndex = 26;
             this.btnPreparedStatement.Text = "GO";
             this.btnPreparedStatement.UseVisualStyleBackColor = true;
@@ -91,10 +92,9 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(16, 203);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(12, 156);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(182, 25);
+            this.label4.Size = new System.Drawing.Size(150, 20);
             this.label4.TabIndex = 27;
             this.label4.Text = "Prepared statement";
             // 
@@ -102,10 +102,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(16, 284);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(12, 218);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(179, 25);
+            this.label2.Size = new System.Drawing.Size(145, 20);
             this.label2.TabIndex = 28;
             this.label2.Text = "Regular expression";
             // 
@@ -113,19 +112,18 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(16, 380);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(12, 279);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(163, 25);
+            this.label3.Size = new System.Drawing.Size(133, 20);
             this.label3.TabIndex = 29;
             this.label3.Text = "Stored procedure";
             // 
             // btnRegularExpression
             // 
-            this.btnRegularExpression.Location = new System.Drawing.Point(713, 273);
-            this.btnRegularExpression.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRegularExpression.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRegularExpression.Location = new System.Drawing.Point(535, 209);
             this.btnRegularExpression.Name = "btnRegularExpression";
-            this.btnRegularExpression.Size = new System.Drawing.Size(160, 49);
+            this.btnRegularExpression.Size = new System.Drawing.Size(120, 40);
             this.btnRegularExpression.TabIndex = 31;
             this.btnRegularExpression.Text = "GO";
             this.btnRegularExpression.UseVisualStyleBackColor = true;
@@ -134,38 +132,36 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(16, 475);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Location = new System.Drawing.Point(12, 358);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(129, 25);
+            this.label6.Size = new System.Drawing.Size(103, 20);
             this.label6.TabIndex = 32;
             this.label6.Text = "SQL injection";
             // 
             // tbSQLInjection
             // 
             this.tbSQLInjection.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbSQLInjection.Location = new System.Drawing.Point(219, 471);
-            this.tbSQLInjection.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbSQLInjection.Location = new System.Drawing.Point(164, 355);
             this.tbSQLInjection.Name = "tbSQLInjection";
-            this.tbSQLInjection.Size = new System.Drawing.Size(465, 30);
+            this.tbSQLInjection.Size = new System.Drawing.Size(350, 26);
             this.tbSQLInjection.TabIndex = 33;
             // 
             // btnStoredProcedure
             // 
-            this.btnStoredProcedure.Location = new System.Drawing.Point(713, 369);
-            this.btnStoredProcedure.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnStoredProcedure.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStoredProcedure.Location = new System.Drawing.Point(535, 270);
             this.btnStoredProcedure.Name = "btnStoredProcedure";
-            this.btnStoredProcedure.Size = new System.Drawing.Size(160, 49);
+            this.btnStoredProcedure.Size = new System.Drawing.Size(120, 40);
             this.btnStoredProcedure.TabIndex = 34;
             this.btnStoredProcedure.Text = "GO";
             this.btnStoredProcedure.UseVisualStyleBackColor = true;
             // 
             // btnSQLInjection
             // 
-            this.btnSQLInjection.Location = new System.Drawing.Point(713, 464);
-            this.btnSQLInjection.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSQLInjection.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSQLInjection.Location = new System.Drawing.Point(535, 349);
             this.btnSQLInjection.Name = "btnSQLInjection";
-            this.btnSQLInjection.Size = new System.Drawing.Size(160, 49);
+            this.btnSQLInjection.Size = new System.Drawing.Size(120, 40);
             this.btnSQLInjection.TabIndex = 36;
             this.btnSQLInjection.Text = "GO";
             this.btnSQLInjection.UseVisualStyleBackColor = true;
@@ -174,10 +170,9 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(16, 111);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(12, 90);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(101, 25);
+            this.label5.Size = new System.Drawing.Size(82, 20);
             this.label5.TabIndex = 38;
             this.label5.Text = "Dropdown";
             // 
@@ -185,18 +180,17 @@
             // 
             this.cbDropdown.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbDropdown.FormattingEnabled = true;
-            this.cbDropdown.Location = new System.Drawing.Point(219, 107);
-            this.cbDropdown.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbDropdown.Location = new System.Drawing.Point(164, 87);
             this.cbDropdown.Name = "cbDropdown";
-            this.cbDropdown.Size = new System.Drawing.Size(465, 33);
+            this.cbDropdown.Size = new System.Drawing.Size(350, 28);
             this.cbDropdown.TabIndex = 37;
             // 
             // btnDropDown
             // 
-            this.btnDropDown.Location = new System.Drawing.Point(713, 100);
-            this.btnDropDown.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDropDown.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDropDown.Location = new System.Drawing.Point(535, 81);
             this.btnDropDown.Name = "btnDropDown";
-            this.btnDropDown.Size = new System.Drawing.Size(160, 49);
+            this.btnDropDown.Size = new System.Drawing.Size(120, 40);
             this.btnDropDown.TabIndex = 39;
             this.btnDropDown.Text = "GO";
             this.btnDropDown.UseVisualStyleBackColor = true;
@@ -204,38 +198,34 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Gainsboro;
-            this.panel1.Location = new System.Drawing.Point(21, 437);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Location = new System.Drawing.Point(12, 328);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(857, 6);
+            this.panel1.Size = new System.Drawing.Size(643, 5);
             this.panel1.TabIndex = 41;
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.Gainsboro;
-            this.panel2.Location = new System.Drawing.Point(16, 69);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel2.Location = new System.Drawing.Point(12, 56);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(857, 6);
+            this.panel2.Size = new System.Drawing.Size(643, 5);
             this.panel2.TabIndex = 42;
             // 
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.Gainsboro;
-            this.panel3.Location = new System.Drawing.Point(16, 533);
-            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel3.Location = new System.Drawing.Point(8, 406);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(857, 6);
+            this.panel3.Size = new System.Drawing.Size(647, 5);
             this.panel3.TabIndex = 42;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(788, 566);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(583, 428);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 20);
+            this.label1.Size = new System.Drawing.Size(72, 16);
             this.label1.TabIndex = 44;
             this.label1.Text = "Recreate";
             // 
@@ -244,21 +234,61 @@
             this.btnReCreate.BackColor = System.Drawing.Color.DarkGray;
             this.btnReCreate.BackgroundImage = global::DBD.Properties.Resources.db;
             this.btnReCreate.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnReCreate.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnReCreate.FlatAppearance.BorderSize = 0;
             this.btnReCreate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnReCreate.Location = new System.Drawing.Point(791, 590);
-            this.btnReCreate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnReCreate.Location = new System.Drawing.Point(586, 457);
             this.btnReCreate.Name = "btnReCreate";
-            this.btnReCreate.Size = new System.Drawing.Size(83, 73);
+            this.btnReCreate.Size = new System.Drawing.Size(62, 59);
             this.btnReCreate.TabIndex = 43;
             this.btnReCreate.UseVisualStyleBackColor = false;
             // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel4.Location = new System.Drawing.Point(665, 56);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(5, 479);
+            this.panel4.TabIndex = 43;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel5.Location = new System.Drawing.Point(676, 56);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(302, 5);
+            this.panel5.TabIndex = 43;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.listBox1.Enabled = false;
+            this.listBox1.Location = new System.Drawing.Point(685, 81);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(293, 327);
+            this.listBox1.TabIndex = 45;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRefresh.Location = new System.Drawing.Point(775, 419);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(120, 35);
+            this.btnRefresh.TabIndex = 46;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkGray;
-            this.ClientSize = new System.Drawing.Size(889, 673);
+            this.ClientSize = new System.Drawing.Size(990, 547);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnReCreate);
             this.Controls.Add(this.panel3);
@@ -280,7 +310,6 @@
             this.Controls.Add(this.tbRegularExpression);
             this.Controls.Add(this.tbPreparedStatement);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Main";
             this.Text = "DBD";
             this.ResumeLayout(false);
@@ -309,6 +338,10 @@
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnReCreate;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnRefresh;
     }
 }
 
